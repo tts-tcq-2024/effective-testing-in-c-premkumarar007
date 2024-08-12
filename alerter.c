aa7ccca9efd9c4a6103af92acc8200c02317e2df
PREM KUMAR R <80571286+premkumarar007@users.noreply.github.com>
2024-08-12 11:31:15 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -25,9 +25,8 @@ void alertInCelcius(float farenheit) {
 
 int main() {
     alertInCelcius(400.5);
-    assert(alertFailureCount == '1');
     alertInCelcius(303.6);
-    assert(alertFailureCount == '1');
+    assert(alertFailureCount == 2);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
