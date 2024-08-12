027e8b098d593367c22f8bba4536d665f5014218
PREM KUMAR R <80571286+premkumarar007@users.noreply.github.com>
2024-08-12 11:06:29 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -17,7 +17,8 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    assert(size(44) == 'M');
+    assert(size(38) == 'S');
+    assert(size(42) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
 }
